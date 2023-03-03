07f095dd5cc0954afe6b231f886bf88c5e73a863
Tino Rusch <tino@kilt.io>
2023-03-03 11:13:03 +0100

    feat: add cookbook recipe how to generate keys (taken from the workshop section)

---
+++ code_examples/sdk_examples/src/core_features/did/00_generate_did_keys.ts
@@ -0,0 +1,32 @@
+import { mnemonicGenerate, mnemonicToMiniSecret } from '@polkadot/util-crypto'
+
+import * as Kilt from '@kiltprotocol/sdk-js'
+
+export function generateKeypairs(mnemonic = mnemonicGenerate()): {
+  authentication: Kilt.KiltKeyringPair & {
+    type: 'ed25519'
+  }
+  encryption: Kilt.KiltEncryptionKeypair
+  attestation: Kilt.KiltKeyringPair
+  delegation: Kilt.KiltKeyringPair
+} {
+  const authentication = Kilt.Utils.Crypto.makeKeypairFromSeed(
+    mnemonicToMiniSecret(mnemonic)
+  )
+  const encryption = Kilt.Utils.Crypto.makeEncryptionKeypairFromSeed(
+    mnemonicToMiniSecret(mnemonic)
+  )
+  const attestation = authentication.derive(
+    '//attestation'
+  ) as Kilt.KiltKeyringPair
+  const delegation = authentication.derive(
+    '//delegation'
+  ) as Kilt.KiltKeyringPair
+
+  return {
+    authentication,
+    encryption,
+    attestation,
+    delegation
+  }
+}
---
+++ docs/develop/01_sdk/02_cookbook/01_dids/00_generate_keys.md
@@ -0,0 +1,20 @@
+---
+id: key-generation
+title: Generate DID keys
+---
+
+import TsJsBlock from '@site/src/components/TsJsBlock';
+
+import GenerateKeys from '!!raw-loader!@site/code_examples/sdk_examples/src/core_features/did/00_generate_did_keys.ts';
+
+Creating a DID requires the generation of some keying material for keys that are to be used for authentication and encryption.
+
+The following is an example of how to create a a set of keypairs suitable for generating a KILT DID from it.
+
+<TsJsBlock>
+  {GenerateKeys}
+</TsJsBlock>
+
+:::info
+This example doesn't show how to store the keys. It is recommended to store the keys in a secure manner, e.g. only storing the private keys encrypted on disk. The mnemonic seed phrase can be used to regenerate the keys, so it is recommended to also store the seed in a secure manner.
+:::
